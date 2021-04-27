20a48c4993198bd9b9364172fca2eea1f035c9e2
rosandicdejan <83307336+rosandicdejan@users.noreply.github.com>
2021-04-27 20:56:57 +0200

    Add files via upload

---
+++ shareposts/app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");
+  define("DB_USER", "ucenik");
+  define("DB_PASS", "1234");
+  define("DB_NAME", "ucenik");
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://37.0.71.52/rosandic.dejan/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  define('APPVERSION','1.0.0');
+  
